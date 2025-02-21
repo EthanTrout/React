daccf66f8ddcd131fe1ee82d57da0f121c564951
EthanTrout <149257299+EthanTrout@users.noreply.github.com>
2025-02-21 17:42:51 +0000

    how to use set state

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,18 +7,21 @@ class StatefulGreeting extends React.Component{
         this.state = {
             introduction: "Hello",
             buttonText: "Exit",
+            count:0,
         };
     }
     handleClick(){
-        this.setState({
-            introduction:"Goodbye!",
-            buttonText:"Enter"
-        });
+        this.setState((prevState,prevProps) =>{
+            return{
+                introduction: prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            }
+        })
     }
     render(){
         return (
         <div>
-            <h1>{this.state.introduction} {this.props.greeting} </h1>
+            <h1>{this.state.introduction} {this.props.greeting} {this.state.count} </h1>
             <button onClick={ () => this.handleClick()}>{this.state.buttonText}</button>
         </div>)
     }
